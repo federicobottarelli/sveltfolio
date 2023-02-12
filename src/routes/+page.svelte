c34fb22488d69ce0b9147b8ae48cdc78586e1f63
Federico Bottarelli <canny891@gmail.com>
2023-02-12 15:32:11 +0100

    tailwind added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,40 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+    <!-- Aside navbar -->
+    <aside class="sticky md:w-[150px] md:flex-shrink-0 mb-8 mx-4 md:mx-0">
+      <div class="sticky lg:top-20">
+        <nav class="flex flex-row md:flex-col relative">
+          <div class="flex flex-row md:flex-col flow-root">
+            <a href="./main.html" class="mr-12 md:mb-4 float-left">Home</a>
+            <div class="flex flex-row md:flex-col float-right">
+                <a href="./about.html" class="mr-4">About me</a>
+                <a href="./works.html">Works</a>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </aside>
+
+    <!-- Main-section -->
+    <div class="flex-col">
+      <!-- Bio -->
+      <div class="mb-12">
+        <p>
+          Welcome! 🧐 I'm Federico, I love everything related to <b>data</b> and
+          their use in real problem. <br />
+          I'm doing a Data Science master's degree in Padua University while
+          working as Junior DataOps Engineer for an Italian company.
+        </p>
+      </div>
+      <hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
+      <div class="font-serif font-semibold text-2xl">
+        <p>My best works</p>
+      </div>
+      <div>
+        <p>
+          Sunt do velit ullamco deserunt nulla incididunt consectetur est. Elit
+          aute aliquip proident deserunt minim excepteur culpa qui velit nulla
+          commodo adipisicing excepteur cillum. Ea duis ullamco commodo
+          incididunt anim ex proident ipsum reprehenderit consectetur veniam. Eu
+          velit reprehenderit anim voluptate.
+        </p>
+      </div>
+    </div>
